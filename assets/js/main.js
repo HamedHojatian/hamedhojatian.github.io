d7f2df2ac7f34e7a558c0f768f713f4d42ec38c9
Hamed Hojatian <hamed.hojatian@polymtl.ca>
2020-07-05 02:01:23 -0400

    update 5 Jul

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,3 +149,11 @@ function setup_nivo_lightbox()
 
     }
 }
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
